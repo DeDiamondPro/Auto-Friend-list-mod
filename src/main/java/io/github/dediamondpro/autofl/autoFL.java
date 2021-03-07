edce77525603eb3c58b13f4686520095b1a2513a
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-03-07 15:24:24 +0100

    added support for leaving and then rejoining

--- src/main/java/io/github/dediamondpro/autofl/autoFL.java
+++ src/main/java/io/github/dediamondpro/autofl/autoFL.java
@@ -7,12 +7,13 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 @Mod(modid = autoFL.MODID, version = autoFL.VERSION)
 public class autoFL
 {
-    public static final String MODID = "autoFL";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "autofl";
+    public static final String VERSION = "1.1";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
@@ -23,15 +24,22 @@ public class autoFL
 
 class Events {
     public boolean send = false;
+    long ticks = 0;
 
     @SubscribeEvent
-    public void onTick(TickEvent.PlayerTickEvent event) {
-        if(!send){
+    public void onTick(TickEvent.ClientTickEvent event) {
+        ticks++;
+        if(!send && ticks % 20 == 0){
             if(isHypixel()){
                 Minecraft.getMinecraft().thePlayer.sendChatMessage("/fl");
                 send = true;
             }
         }
+    }
+
+    @SubscribeEvent
+    public void onDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent event){
+        send = false;
     }
 
     public boolean isHypixel() {
